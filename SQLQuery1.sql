b8e44d67ef3ef3a459b2709e7c581b63e0f211ee
parthinarayanan <114048751+parthinarayanan@users.noreply.github.com>
2022-12-27 13:30:00 +0530

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,7 @@
+create database Empfamily 
+use Empfamily 
+create table  Empfamily ( EmpID int identity(1,1),EMPLOYEE_CODE varchar(100),Dependency_TYPE varchar(100),
+Dependency_DOB varchar(100),AGE int,Dependency_occupation varchar(100),Dependency_AadhaarNumber varchar(100),
+Dependency_PAN_Number varchar(100))
+
+select * from Empfamily
